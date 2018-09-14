9330692c35f5a359a721133b9fc58b6c4ee0adb0
wangxiao <xiao.wang@fnvalley.com>
2018-09-14 13:57:54 +0800

    init

---
+++ Assets/QctreeNode.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QctreeNode
+{
+    const int QT_NODE_CAPACITY = 4;
+    const int maxLevel = 8;
+    int level;
+    
+    public Bounds bounds;
+    public List<GameObject> gameObjectsList;
+    public QctreeNode[] childNodes;
+
+    public QctreeNode(Bounds b)
+    {
+        bounds = b;
+        gameObjectsList = new List<GameObject>();
+        // childNodes = new List<QctreeNode>();
+        level = 0;
+    }
+    public QctreeNode(Bounds b, int lev)
+    {
+        bounds = b;
+        gameObjectsList = new List<GameObject>();
+        //childNodes = null;
+        level = lev;
+    }
+    public bool Insert(GameObject go)
+    {
+        if (!bounds.Contains(go.transform.position))
+        {
+            return false;
+        }
+
+        if (gameObjectsList.Count < QT_NODE_CAPACITY)
+        {
+            gameObjectsList.Add(go);
+            return true;
+        }
+
+        if (childNodes == null)
+            SubDivide();
+
+        //foreach(QctreeNode node in childNodes)
+        //{
+        //    if (node.Insert(go)) return true;
+        //}
+
+        if (childNodes == null) { Debug.Log("Out of Capacity"); return false; }
+        for (int i = 0; i < 8; i++)
+        {
+            if (childNodes[i].Insert(go)) return true;
+        }
+
+        return false;
+    }
+
+    void SubDivide()
+    {
+       
+        if (childNodes == null && level < maxLevel)
+        //if (childNodes == null)
+        {
+            Debug.Log("SubDivide");
+
+            childNodes = new QctreeNode[8];
+            float halfSizeX = bounds.size.x / 4;
+            float halfSizeY = bounds.size.y / 4;
+            float halfSizeZ = bounds.size.z / 4;
+            Vector3 centerNew = new Vector3(bounds.center.x - halfSizeX, bounds.center.y + halfSizeY, bounds.center.z + halfSizeZ);
+            Bounds boundsNew = new Bounds(centerNew, bounds.size / 2);
+            childNodes[0] = new QctreeNode(boundsNew,level +1);//up left back 
+
+            centerNew = new Vector3(bounds.center.x - halfSizeX, bounds.center.y + halfSizeY, bounds.center.z - halfSizeZ);
+            boundsNew = new Bounds(centerNew, bounds.size / 2);
+            childNodes[1] = new QctreeNode(boundsNew, level + 1);//up left front 
+
+            centerNew = new Vector3(bounds.center.x + halfSizeX, bounds.center.y + halfSizeY, bounds.center.z - halfSizeZ);
+            boundsNew = new Bounds(centerNew, bounds.size / 2);
+            childNodes[2] = new QctreeNode(boundsNew, level + 1);//up right front
+
+            centerNew = new Vector3(bounds.center.x + halfSizeX, bounds.center.y + halfSizeY, bounds.center.z + halfSizeZ);
+            boundsNew = new Bounds(centerNew, bounds.size / 2);
+            childNodes[3] = new QctreeNode(boundsNew, level + 1);//up right back
+
+            centerNew = new Vector3(bounds.center.x - halfSizeX, bounds.center.y - halfSizeY, bounds.center.z + halfSizeZ);
+            boundsNew = new Bounds(centerNew, bounds.size / 2);
+            childNodes[4] = new QctreeNode(boundsNew, level + 1);//bottom left back 
+
+            centerNew = new Vector3(bounds.center.x - halfSizeX, bounds.center.y - halfSizeY, bounds.center.z - halfSizeZ);
+            boundsNew = new Bounds(centerNew, bounds.size / 2);
+            childNodes[5] = new QctreeNode(boundsNew, level + 1);//bottom left front 
+
+            centerNew = new Vector3(bounds.center.x + halfSizeX, bounds.center.y - halfSizeY, bounds.center.z - halfSizeZ);
+            boundsNew = new Bounds(centerNew, bounds.size / 2);
+            childNodes[6] = new QctreeNode(boundsNew, level + 1);//bottom right front
+
+            centerNew = new Vector3(bounds.center.x + halfSizeX, bounds.center.y - halfSizeY, bounds.center.z + halfSizeZ);
+            boundsNew = new Bounds(centerNew, bounds.size / 2);
+            childNodes[7] = new QctreeNode(boundsNew, level + 1);//bottom right back
+        }
+    }
+
+    public List<GameObject> preOrder(QctreeNode node)
+    {
+        if (node == null) return null;
+        List<GameObject> orderGoList = new List<GameObject>();
+
+        for (int g = 0; g < node.gameObjectsList.Count; g++)
+        {
+                orderGoList.Add(node.gameObjectsList[g]);
+        }
+
+        if (childNodes == null) return orderGoList;
+
+        for (int i = 0; i < 8; i++)
+        {
+            orderGoList.AddRange(childNodes[i].preOrder(childNodes[i]));
+            //Debug.Log("for queryGoList: " + queryGoList.Count);
+        }
+
+        return orderGoList;
+    }
+
+    public List<GameObject> QueryInBounds(Bounds bds)
+    {
+        List<GameObject> queryGoList = new List<GameObject>();
+        if (!bounds.Intersects(bds)) return queryGoList;// empty list
+
+        for (int g = 0; g < gameObjectsList.Count; g++)
+        {
+            //if(bds.Intersects(gameObjectsList[g].GetComponent<Renderer>().bounds))
+            if (bds.Contains(gameObjectsList[g].transform.position))          
+            {
+                
+                queryGoList.Add(gameObjectsList[g]);
+            }
+        }
+
+        if (childNodes == null) return queryGoList;
+        //Debug.Log("queryGoList: " + queryGoList.Count);
+        for (int i = 0; i < 8; i++)
+        {
+            queryGoList.AddRange(childNodes[i].QueryInBounds(bds));
+            //Debug.Log("for queryGoList: " + queryGoList.Count);
+        }
+
+        return queryGoList;
+    }
+
+}
---
+++ Assets/SpawnObjects.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour {
+    public GameObject go;
+    public   int GoNum = 25;
+    [Tooltip("显示八叉树分割空间")]
+    public bool isShowQctreeBox;
+    List<GameObject> listGo = new List<GameObject>();
+
+    QctreeNode root;
+
+    Bounds queryBound;
+    Vector3 queryCenter;
+    List<GameObject> listOrder;
+    // Use this for initialization
+    void Start () {
+        
+        Spawns();
+
+        root = new QctreeNode(new Bounds(Vector3.zero, new Vector3(100, 100, 100)));
+
+        foreach (GameObject g in listGo)
+        {
+            root.Insert(g);
+        }
+
+        //queryCenter = new Vector3(100, 100, 100);
+        queryBound = new Bounds(queryCenter, new Vector3(30, 30, 30));
+
+
+        int nnn = root.QueryInBounds(queryBound).Count;
+        Debug.Log("Count: " + nnn);
+
+        listOrder = root.preOrder(root);
+        Debug.Log(listOrder.Count);
+    }
+
+    void Spawns()
+    {
+        for (int i = 0; i < GoNum; i++)
+        {
+            Vector3 p = new Vector3(Random.Range(-50, 50), Random.Range(-50, 50), Random.Range(-50, 50));
+            GameObject tmpGo = Instantiate(go, p, Quaternion.identity);
+            tmpGo.name += i.ToString();
+            listGo.Add(tmpGo);        
+        }
+
+    }
+
+    List<GameObject> goList;
+    void unLoadNotInside(Bounds bds)
+    {
+        if (goList == null) return;
+        foreach(GameObject g in goList)
+        {
+            if(!bds.Contains(g.transform.position))
+            {
+                g.GetComponent<MeshRenderer>().material.color = Color.white;
+            }
+        }
+    }
+  void loadGo()
+    {
+        foreach (GameObject g in listGo)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        if(h!=0 || v !=0)
+        {
+            queryCenter += new Vector3( h*2, queryCenter.y, v * 2);
+
+         
+        }
+
+        //if(Input.GetKeyDown(KeyCode.Q))
+        {
+            queryBound = new Bounds(queryCenter, new Vector3(30, 30, 50));
+
+            unLoadNotInside(queryBound);
+
+           
+           goList = root.QueryInBounds(queryBound);
+            
+            foreach(GameObject g in goList)
+            {
+                g.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            foreach (GameObject g in root.gameObjectsList)
+            {
+                Debug.Log(g.name);
+            }
+
+        }
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Gizmos.DrawWireCube(Vector3.zero, new Vector3(100, 100, 100));
+        if (root != null)
+        {
+            Gizmos.DrawWireCube(root.bounds.center, root.bounds.size);
+
+            //DrawDebug();
+            //Gizmos.DrawWireCube(root.childNodes[0].bounds.center, root.childNodes[0].bounds.size);
+            // Gizmos.DrawWireCube(root.childNodes[1].bounds.center, root.childNodes[1].bounds.size);
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(queryCenter, queryBound.size);
+        }
+        if(listOrder != null)
+        {
+            Gizmos.color = Color.blue;
+            foreach (GameObject g in listOrder)
+            {
+                Gizmos.DrawWireCube(g.transform.position, g.GetComponent<Renderer>().bounds.size);
+            }
+        }
+
+        if(isShowQctreeBox) DrawDebug(root);
+    }
+
+    void DrawDebug(QctreeNode nodeTree)
+    {
+        if (nodeTree == null) return;
+        Gizmos.color = Color.cyan;
+        if (nodeTree.childNodes == null) return;
+        foreach (QctreeNode node in nodeTree.childNodes)
+        {
+            //Debug.Log(node.bounds.center);
+            Gizmos.DrawWireCube(node.bounds.center, node.bounds.size);
+
+            if (node.childNodes != null)
+            {
+                foreach (QctreeNode chileNode in node.childNodes)
+                {
+                    DrawDebug(chileNode);
+                }
+            }
+        }
+    }
+
+   
+}
